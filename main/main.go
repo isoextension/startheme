f8f474559bb75b3405d83d32c7e8b64982613594
Maximo Kelm <isoextension@gmail.com>
2026-01-20 14:51:37 -0300

    add multiple utility files

--- main/main.go
+++ main/main.go
@@ -1,5 +1,13 @@
 package main
 
-import "github.com/isoextension/btgo/taylog"
+import (
+	"runtime"
 
-var log *taylog.Logger = taylog.New()
+	"github.com/isoextension/btgo/taylog"
+)
+
+var log *taylog.Logger = taylog.New("startheme", nil)
+
+func main() {
+	debugf("Starting Startheme v%s with Go version %s\n", version, runtime.Version())
+}
